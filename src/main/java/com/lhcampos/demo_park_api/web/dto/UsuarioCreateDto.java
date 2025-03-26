a94b7f3da9f87a91207540d1d8530397199a0532
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-03-26 15:51:56 -0300

    feat: validação para criar usuario

--- src/main/java/com/lhcampos/demo_park_api/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/lhcampos/demo_park_api/web/dto/UsuarioCreateDto.java
@@ -1,5 +1,8 @@
 package com.lhcampos.demo_park_api.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter
@@ -8,6 +11,10 @@ import lombok.*;
 @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email( message = "formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9,-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 }
