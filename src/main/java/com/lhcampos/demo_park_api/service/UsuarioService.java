4d4f8a78327c571a68e4fee0450c31870600ee35
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-03-28 14:30:26 -0300

    feat: exception username ja existe

--- src/main/java/com/lhcampos/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/lhcampos/demo_park_api/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package com.lhcampos.demo_park_api.service;
 
 import com.lhcampos.demo_park_api.entity.Usuario;
+import com.lhcampos.demo_park_api.exception.UsernameUniqueViolationException;
 import com.lhcampos.demo_park_api.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,12 @@ public class UsuarioService {
     @Transactional
     public Usuario salvar(Usuario usuario) {
 
-        return usuarioRepository.save(usuario);
+        try {
+            return usuarioRepository.save(usuario);
+        } catch (org.springframework.dao.DataIntegrityViolationException ex) {
+            throw new UsernameUniqueViolationException(String
+                    .format("Username {%s} já cadastrado", usuario.getUsername()));
+        }
     }
 
     @Transactional(readOnly = true)
