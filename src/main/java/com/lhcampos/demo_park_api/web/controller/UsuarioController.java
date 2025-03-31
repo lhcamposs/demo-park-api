4d07bcbbbb4e0ee9d103a337245b81bf8efd3bde
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-03-31 16:32:24 -0300

    📚 docs: documentando recuperar usuario pelo id

--- src/main/java/com/lhcampos/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/lhcampos/demo_park_api/web/controller/UsuarioController.java
@@ -45,6 +45,14 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
+    @Operation(summary = "Recuperar um usuario pelo id", description = "Recuperar um usuario pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso recuperado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
