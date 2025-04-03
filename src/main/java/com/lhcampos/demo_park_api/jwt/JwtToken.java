fd0f052c839fd31b9af5a239b1a162cc9a3742d2
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-04-03 17:07:19 -0300

    utils: jwt token e jwtUtils

---
+++ src/main/java/com/lhcampos/demo_park_api/jwt/JwtToken.java
@@ -0,0 +1,13 @@
+package com.lhcampos.demo_park_api.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class JwtToken {
+    private String token;
+}
