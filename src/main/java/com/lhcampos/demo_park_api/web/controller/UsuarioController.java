0a92eb1fdfd33196e17c21d84f9831393df47a76
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-04-08 18:11:23 -0300

    feat: permissao de acesso a listage para admin

--- src/main/java/com/lhcampos/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/lhcampos/demo_park_api/web/controller/UsuarioController.java
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -54,6 +55,7 @@ public class UsuarioController {
             }
     )
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
