fa827cbcfa809260fa8109fb71bbaa1ed8638506
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-04-09 17:09:55 -0300

    fix: liberando acesso ao recursos da documentação

--- src/main/java/com/lhcampos/demo_park_api/config/SpringSecurityConfig.java
+++ src/main/java/com/lhcampos/demo_park_api/config/SpringSecurityConfig.java
@@ -23,6 +23,14 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @Configuration
 public class SpringSecurityConfig {
 
+    private static final String[] DOCUMENTATION_OPENAPI = {
+            "/docs/index.html",
+            "/docs-park.html", "/docs-park/**",
+            "/v3/api-docs/**",
+            "/swagger-ui-custom.html", "/swagger-ui.html", "/swagger-ui/**",
+            "/**.html", "/webjars/**", "/configuration/**", "/swagger-resources/**"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -32,6 +40,15 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(antMatcher(HttpMethod.POST, "/api/v1/usuarios")).permitAll()
                         .requestMatchers(antMatcher(HttpMethod.POST, "/api/v1/auth")).permitAll()
+                        .requestMatchers(
+                                antMatcher(HttpMethod.POST, "/api/v1/usuarios"),
+                                antMatcher(HttpMethod.POST, "/api/v1/auth"),
+                                antMatcher("/docs-park.html"),
+                                antMatcher("/docs-park/**"),
+                                antMatcher("/swagger-ui.html"),
+                                antMatcher("/swagger-ui/**"),
+                                antMatcher("/webjars/**")
+                        ).permitAll()
                         .anyRequest().authenticated()
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
