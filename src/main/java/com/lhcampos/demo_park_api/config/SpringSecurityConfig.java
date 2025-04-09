1ed5166329145bbeca92ffb31e82913dded14fea
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-04-09 16:41:16 -0300

    feat: tratamento de erros 401 e 403

--- src/main/java/com/lhcampos/demo_park_api/config/SpringSecurityConfig.java
+++ src/main/java/com/lhcampos/demo_park_api/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.lhcampos.demo_park_api.config;
 
+import com.lhcampos.demo_park_api.jwt.JwtAuthenticationEntryPoint;
 import com.lhcampos.demo_park_api.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,8 +37,9 @@ public class SpringSecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(
                         jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
-                )
-                .build();
+                ).exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((new JwtAuthenticationEntryPoint()))
+                ).build();
     }
 
     @Bean
