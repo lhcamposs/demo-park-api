172416570e0f22d40f94bd2ee3438f98e8bace58
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-04-10 17:24:32 -0300

    test: autenticação com status 200

--- src/test/resources/sql/usuarios/usuarios-insert.sql
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -1,6 +1,6 @@
 insert into USUARIOS (id, username, password, role)
-    values (100, 'ana@email.com', '123456', ' ROLE_ADMIN');
+    values (100, 'ana@email.com', '$2a$12$wTid126TXSdUlcuQq/vUNeUMtKFqtyztbkmdZ19jWBZ1di5ap8fkC', ' ROLE_ADMIN');
 insert into USUARIOS (id, username, password, role)
-values (101, 'bia@email.com', '123456', ' ROLE_CLIENTE');
+values (101, 'bia@email.com', '$2a$12$wTid126TXSdUlcuQq/vUNeUMtKFqtyztbkmdZ19jWBZ1di5ap8fkC', ' ROLE_CLIENTE');
 insert into USUARIOS (id, username, password, role)
-values (102, 'bob@email.com', '123456', ' ROLE_CLIENTE');
+values (102, 'bob@email.com', '$2a$12$wTid126TXSdUlcuQq/vUNeUMtKFqtyztbkmdZ19jWBZ1di5ap8fkC', ' ROLE_CLIENTE');
