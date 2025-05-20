83443052945fc61b0269b978b05fc74a19e45ba4
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-05-20 16:39:48 -0300

    test: busca pelo pefil cliente status 200 e 403

--- src/test/java/com/lhcampos/demo_park_api/EstacionamentoIT.java
+++ src/test/java/com/lhcampos/demo_park_api/EstacionamentoIT.java
@@ -268,4 +268,50 @@ public class EstacionamentoIT {
                 .jsonPath("path").isEqualTo("/api/v1/estacionamentos/cpf/34304891057")
                 .jsonPath("method").isEqualTo("GET");
     }
+
+    @Test
+    public void buscarEstacionamentos_DoClienteLogado_RetornarSucessoStatus200() {
+
+        PageableDto responseBody = testClient.get()
+                .uri("/api/v1/estacionamentos?size=1&page=0")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bob@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNumber()).isEqualTo(0);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+
+        responseBody = testClient.get()
+                .uri("/api/v1/estacionamentos?size=1&page=1")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "bob@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getContent().size()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getNumber()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(responseBody.getSize()).isEqualTo(1);
+    }
+
+    @Test
+    public void buscarEstacionamentos_DoClienteLogadoComPerfilAdmin_RetornarErrorStatus403() {
+
+        testClient.get()
+                .uri("/api/v1/estacionamentos")
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo("403")
+                .jsonPath("path").isEqualTo("/api/v1/estacionamentos")
+                .jsonPath("method").isEqualTo("GET");
+    }
 }
