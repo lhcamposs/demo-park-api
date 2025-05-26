8d7f8ff66f8e4be5c5cd73840f82242a645ca73f
Luan Henique Campos Soares <101595196+lhcamposs@users.noreply.github.com>
2025-05-26 16:10:52 -0300

    feat: controller para consumir relatorio
    Adicionando recurso no controller para consumir o relatório por cpf

--- src/main/java/com/lhcampos/demo_park_api/web/controller/EstacionamentoController.java
+++ src/main/java/com/lhcampos/demo_park_api/web/controller/EstacionamentoController.java
@@ -3,8 +3,10 @@ package com.lhcampos.demo_park_api.web.controller;
 import com.lhcampos.demo_park_api.entity.ClienteVaga;
 import com.lhcampos.demo_park_api.jwt.JwtUserDetails;
 import com.lhcampos.demo_park_api.repository.projection.ClienteVagaProjection;
+import com.lhcampos.demo_park_api.service.ClienteService;
 import com.lhcampos.demo_park_api.service.ClienteVagaService;
 import com.lhcampos.demo_park_api.service.EstacionamentoService;
+import com.lhcampos.demo_park_api.service.JasperService;
 import com.lhcampos.demo_park_api.web.dto.EstacionamentoCreateDto;
 import com.lhcampos.demo_park_api.web.dto.EstacionamentoResponseDto;
 import com.lhcampos.demo_park_api.web.dto.PageableDto;
@@ -20,6 +22,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -27,12 +30,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
 import java.net.URI;
 
 import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
@@ -45,6 +50,8 @@ import static io.swagger.v3.oas.annotations.enums.ParameterIn.QUERY;
 public class EstacionamentoController {
     private final EstacionamentoService estacionamentoService;
     private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final JasperService jasperService;
 
     @Operation(summary = "Operação de check-in", description = "Recurso para dar entrada de um veículo no estacionamento. " +
             "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
@@ -199,5 +206,19 @@ public class EstacionamentoController {
         Page<ClienteVagaProjection> projection = clienteVagaService.buscarTodosPorUsuarioId(user.getId(), pageable);
         PageableDto dto = PageableMapper.toDto(projection);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/relatorio")
+    @PreAuthorize("hasRole('CLIENTE')")
+    public ResponseEntity<Void> getRelatorio(HttpServletResponse response, @AuthenticationPrincipal JwtUserDetails user) throws IOException {
+        String cpf = clienteService.buscarPorUsuarioId(user.getId()).getCpf();
+        jasperService.addParams("CPF", cpf);
+
+        byte[] bytes = jasperService.gerarPdf();
+
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        response.setHeader("Content-Disposition", "inline; filename=" + System.currentTimeMillis() + ".pdf");
+        response.getOutputStream().write(bytes);
+        return ResponseEntity.ok().build();
     }
 }
